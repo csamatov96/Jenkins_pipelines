c1ddb3d3ba09ab84707db7b895110cc159b7925f
Chyngyzkan Samatov <50221223+csamatov96@users.noreply.github.com>
2020-02-09 17:15:27 -0600

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,8 @@
 node {
     properties(
         properties([parameters([
-        choice(choices: ['golden_ami', 'tower', 'elk', 'nagiosxi ', 'gitlab ', 
-		'nexus ', 'vault '], 
-        description: 'What tool would u like to build? ', name: 
-		'TOOL_TO_PROVISION'), 
+        choice(choices: ['golden_ami', 'tower', 'elk', 'nagiosxi ', 'gitlab ', 'nexus ', 'vault '], 
+        description: 'What tool would u like to build? ', name: 'TOOL_TO_PROVISION'), 
         choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], 
         description: 'Please choose a region ', name: 'AMI_REGION')])])
 
@@ -14,17 +12,21 @@ node {
     }
     stage("Build Image"){
         sh "packer version"
-        //sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
+        sh "packer build -var region=${AMI_REGION} tools/${TOOL_TO_PROVISION}.json"
         
     }
     stage("Send Notification to Slack"){
-        slackSend channel: 'nagios_alerts', message: 'Golden AMI has been built '
+        slackSend channel: 'nagios_alerts', message: "=${TOOL_TO_PROVISION} has been built"
         
     }
     stage("Send Email"){
-        mail bcc: '', body: '''Hello, Your AMI is ready in us-east-1 
-        Thanks ''', cc: '', from: '', replyTo: '', subject: 'Golden AMI has been 
-built', to: '996bkg@gmail.com'
+        mail bcc: '', 
+        body: """Hello, Your AMI is ready in ${AMI_REGION} Thanks""", 
+        cc: '', 
+        from: '', 
+        replyTo: '', 
+        subject: "${TOOL_TO_PROVISION}.json has been built", 
+        to: '996bkg@gmail.com'
         
     }
 }
