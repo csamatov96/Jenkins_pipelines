9403bc01fb0bac71c274cef81c22724f0d5d2371
csamatov96 <csamatov96@gmail.com>
2020-02-09 17:53:15 -0600

    sent

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,22 +1,33 @@
 node {
-    properties(
-        properties([parameters([
-        choice(choices: ['golden_ami', 'tower', 'elk', 'nagiosxi ', 'gitlab ', 'nexus ', 'vault '], description: 'What tool would u like to build? ', name: 'TOOL_TO_PROVISION'), 
-        choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please choose a region ', name: 'AMI_REGION')])])
+    properties([parameters([
+        choice(choices: [
+            'golden_ami', 
+            'tower', 
+            'elk', 
+            'nagiosxi', 
+            'gitlab', 
+            'nexus', 
+            'vault'
+            ], 
+            description: 'What tool would you like to build?', name: 'TOOL_TO_PROVISION'),
+        text(defaultValue: 'dummy@gmail.com', description: 'Please provide email(s) for notifications. Use , for multiple emails', name: 'EMAIL_TO_SEND'),
+        choice(choices: [
+            'us-east-1', 
+            'us-east-2', 
+            'us-west-1', 
+            'us-west-2'
+            ], description: 'Please choose a region', name: 'AMI_REGION')])])
 
     stage("Pull Repo"){
-        git 'https://github.com/csamatov96/packer.git' 
+        git 'https://github.com/farrukh90/packer.git'
     }
-
     stage("Build Image"){
         sh "packer version"
-        //sh "packer build -var region=${AMI_REGION} tools/${TOOL_TO_PROVISION}.json"
+        //sh "packer  build -var region=${AMI_REGION} tools/${TOOL_TO_PROVISION}.json"
     }
-
     stage("Send Notification to Slack"){
         slackSend channel: 'nagios_alerts', message: "${TOOL_TO_PROVISION} has been built"
     }
-
     stage("Send Email"){
         mail bcc: '', 
         body: "Hello, Your AMI is ready in ${AMI_REGION} Thanks", 
@@ -24,7 +35,7 @@ node {
         from: '', 
         replyTo: '', 
         subject: "${TOOL_TO_PROVISION} has been built", 
-        to: '996bkg@gmail.com'
+        to: "${email_to_send}"
     }
 }
 
