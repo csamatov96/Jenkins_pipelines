dd9daf76f7dff2894394a2dd437176a05eaf621f
csamatov96 <csamatov96@gmail.com>
2020-02-09 18:43:54 -0600

    stage filled

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -3,14 +3,15 @@ node{
         git 'https://github.com/csamatov96/terraform-iaac-eks-burak.git' 
 
     }
-    stage("stage1"){
+    stage("Download Terraform"){
         sh "terraform version"
         sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
         sh "unzip terraform_0.12.19_linux_amd64.zip"
         sh "./terraform version"
         
     }
-    stage("stage1"){
+    stage("Set Backend"){
+        sh "source setenv.sh configurations/dev/us-east-1/dev.tfvars"
         
     }
     stage("stage1"){
