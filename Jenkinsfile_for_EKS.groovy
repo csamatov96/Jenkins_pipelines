1a4d11c6f1deb6c25c310d6a7ccb82e43651846f
csamatov96 <csamatov96@gmail.com>
2020-02-09 18:50:41 -0600

    added

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -1,30 +1,25 @@
 node{
     stage("Pull Repo"){
         ws ("tmp/"){
-            git 'https://github.com/csamatov96/terraform-iaac-eks-burak.git' 
+            git 'https://github.com/enver0173/terraform-iaac-eks-burak'
         }
-
+        
     }
     stage("Download Terraform"){
-        ws ("tmp/"){
+        ws ("tmp/") {
             sh "terraform version"
             sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
-            sh "unzip terraform_0.12.19_linux_amd64.zip"
-            sh "./terraform version"
-        }
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
     }
     stage("Set Backend"){
-        ws "tmp/" {
+        ws ("tmp/"){
             sh "./terraform init"
-
         }
-        //sh "source setenv.sh configurations/dev/us-east-1/dev.tfvars"
-        
     }
     stage("Plan"){
         ws ("tmp/") {
-            sh "./terraform plan -var-file configurations/dev/us-east-1/dev.tfvars"
+            sh "./terraform plan -var-file configurations/dev/us-west-2/dev.tfvars"
         }
-        
     }
 }
